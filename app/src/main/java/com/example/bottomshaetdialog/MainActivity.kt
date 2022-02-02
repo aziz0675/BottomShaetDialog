7d7aa0e2a5e5d250f4928eafa458c17ad872625a
Aziz Xushvaqtov <95211018+aziz0675@users.noreply.github.com>
2022-02-02 12:37:17 +0500

    ButtomSheet. Finished

--- app/src/main/java/com/example/bottomshaetdialog/MainActivity.kt
+++ app/src/main/java/com/example/bottomshaetdialog/MainActivity.kt
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.LinearLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomSheet: BottomSheetBehavior<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupUI()
+        showCardPayment()
     }
 
     @SuppressLint("ResourceType")
@@ -25,13 +27,38 @@ class MainActivity : AppCompatActivity() {
         val payment_btn = findViewById<Button>(R.id.payment_btn)
         val sheetBehavior =  BottomSheetBehavior.from(bottomSheet)
 
-        payment_btn.setOnClickListener{
-            if (sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
-                sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            }else{
-                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-            }
+        sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
 
+        payment_btn.setOnClickListener{
+            if (sheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
+                sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED // peek bo'yicha joyiga qaytarish
+            }else{
+                sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+        }
+
+    }
+
+    private fun showCardPayment() {
+        var cash_layout = findViewById<LinearLayout>(R.id.cash_layout)
+        var card_layout = findViewById<LinearLayout>(R.id.card_layout)
+        var card_checkbox = findViewById<CheckBox>(R.id.card_checkbox)
+        var cash_checkbox = findViewById<CheckBox>(R.id.cash_checkbox)
+        cash_layout.setOnClickListener {
+            if (cash_checkbox.isChecked) {
+                cash_checkbox.isChecked = false
+            } else {
+                cash_checkbox.isChecked = true
+                card_checkbox.isChecked = false
+            }
+        }
+        card_layout.setOnClickListener {
+            if (card_checkbox.isChecked) {
+                card_checkbox.isChecked = false
+            } else {
+                card_checkbox.isChecked = true
+                cash_checkbox.isChecked = false
+            }
         }
     }
 }
